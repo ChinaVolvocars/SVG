750a4df1a537f2c559ab780c9998cb1ea453f1bb
MiHiTiger <AtlantisSpeed@gmail.com>
2016-07-10 21:29:08 +0800

    Initial commit

---
+++ msvg/src/main/java/com/meahu/msvg/MainActivity.java
@@ -0,0 +1,48 @@
+package com.meahu.msvg;
+
+import android.graphics.Path;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+public class MainActivity extends FragmentActivity {
+
+    private PathView pathView;
+    private Path path;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pathView = (PathView) findViewById(R.id.pathView);
+        path = makeConvexArrow(50, 100);
+        pathView.setPath(path);
+        pathView.setFillAfter(true);
+        pathView.useNaturalColors();
+        pathView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pathView.getPathAnimator().
+                        delay(100).
+                        duration(1500).
+                        interpolator(new AccelerateDecelerateInterpolator()).
+                        start();
+            }
+        });
+    }
+
+    private Path makeConvexArrow(float length, float height) {
+        final Path path = new Path();
+        path.moveTo(0.0f, 0.0f);
+        path.lineTo(length / 4f, 0.0f);
+        path.lineTo(length, height / 2.0f);
+        path.lineTo(length / 4f, height);
+        path.lineTo(0.0f, height);
+        path.lineTo(length * 3f / 4f, height / 2f);
+        path.lineTo(0.0f, 0.0f);
+        path.close();
+        return path;
+    }
+}
